6ea1adab65c3e21a6c3dd6063044072843b25900
ashif <ashifkhn02@gmail.com>
2023-06-16 00:50:22 +0530

    Angram problem

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,29 @@
 */
 
 function isAnagram(str1, str2) {
+	if (str1.length !== str2.length) {
+		return false
+	}
+	let count =new Map()
+	for (let letter of str1) {
 
+			count.set(letter, (count.get(letter)||0) + 1)
+
+	}
+
+		console.log('second for loop')
+	for (let item of str1) {
+		if (!count.has(item)) {
+			return false
+		}
+		count.set(item, count.get(item) - 1)
+		if(count.get(item)===0){
+			count.delete(item)
+		}
+  }
+
+  return true;
 }
+console.log(isAnagram('ashif', 'qhesj'))
 
-module.exports = isAnagram;
+module.exports = isAnagram
